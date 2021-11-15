efb99f617cb015938a02df97b1a00856526d84a6
pnarlyyew@mail.ru <65395142+NarParahat@users.noreply.github.com>
2021-11-15 16:13:38 +0300

    String -> int

--- timetable2021/src/com/parahat/Audience.java
+++ timetable2021/src/com/parahat/Audience.java
@@ -2,8 +2,8 @@ package com.parahat;
 
 public class Audience {
     String address;
-    String capacityPeople;
-    public Audience(String address, String capacityPeople){
+    int capacityPeople;
+    public Audience(String address, int capacityPeople){
         this.address=address;
         this.capacityPeople=capacityPeople;
     }
